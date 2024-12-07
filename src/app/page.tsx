dc65e3df0c007a46300f05fc14fb6459f7fd9e8c
arburhan <burhan128454@outlook.com>
2024-12-07 21:36:20 +0600

    add react flow, timeline package & education section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 
 import Banner from "components/Banner";
+import Education from "components/Education/Education";
 import NavbarComp from "components/NavbarComp";
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <div className="p-5 fonts-sans">
       <NavbarComp />
       <Banner />
+      <Education />
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
 
       </footer>
---
+++ src/components/Education/Education.tsx
@@ -0,0 +1,60 @@
+'use client';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import { IoSchoolSharp } from 'react-icons/io5';
+
+const Education = () => {
+    return (
+        <div>
+            <VerticalTimeline>
+                <VerticalTimelineElement
+                    iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
+                    icon={<IoSchoolSharp />}
+                    className="vertical-timeline-element--education"
+                    contentStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
+                    contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+                    date="2022 - Present"
+                >
+                    <h3 className="vertical-timeline-element-title text-xl font-bold">Varendra University</h3>
+                    <address className="vertical-timeline-element-subtitle">Rajshahi Bypass Road, Chandrima, Paba, Rajshahi-6204</address>
+                    <h4 className="vertical-timeline-element-degree font-bold">BSc in CSE</h4>
+                    <p>
+                        Pursuing B.Sc. in Computer Science & Engineering with a focus on software development, problem-solving, and cutting-edge technologies. Gaining hands-on experience in programming, database management, web development, and data structures. Actively involved in academic projects and eager to contribute to innovative solutions in the tech industry.
+                    </p>
+                </VerticalTimelineElement>
+                <VerticalTimelineElement
+                    className="vertical-timeline-element--education"
+                    date="August 2017 - August 2021"
+                    contentStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+                    iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+                    icon={<IoSchoolSharp />}
+                >
+                    <h3 className="vertical-timeline-element-title text-xl font-bold">Rajshahi Polytechnic Institute</h3>
+                    <address className="vertical-timeline-element-subtitle">Sopura, Chandrima, Rajshahi, Bangladesh</address>
+                    <h4 className="vertical-timeline-element-degree font-bold">Diploma in Computer Technology </h4>
+                    <p>
+                        Completed a comprehensive diploma program covering computer hardware, software, and networking. Gained practical experience through hands-on projects, technical training, and foundational skills in programming and hardware knowledge. This program provided a strong base for further studies in computer science.
+                    </p>
+                </VerticalTimelineElement>
+                <VerticalTimelineElement
+                    className="vertical-timeline-element--education"
+                    date="January 2012 - February 2017"
+                    contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                    icon={<IoSchoolSharp />}
+                >
+                    <h3 className="vertical-timeline-element-title text-xl font-bold">Kaligram Dodangi High School</h3>
+                    <address className="vertical-timeline-element-subtitle">Manda, Naogaon, Rajshahi, Bangladesh</address>
+                    <h4 className="vertical-timeline-element-degree font-bold">Science </h4>
+                    <p>
+                        Completed high school education from Kaligram Dodangi High School with a strong focus on the Science discipline, laying the groundwork for future academic and technical pursuits.
+                    </p>
+
+                </VerticalTimelineElement>
+            </VerticalTimeline>
+
+        </div>
+    );
+};
+
+export default Education;
